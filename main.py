8fd599714c658678dff5bd75b44c6e1c0422df5f
Sean <trippiscoding@gmail.com>
2025-07-20 12:56:18 -0400

    added majority of the user auth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from database import Base, engine
+from auth import auth_router
 
 app = FastAPI()
+app.include_router(auth_router, prefix='/auth', tags=['auth'])
 
 Base.metadata.create_all(bind=engine)
--- models.py
+++ models.py
@@ -1,4 +1,33 @@
-from sqlalchemy import Integer, String, Date, Column
-from typing import Annotated
-from pydantic import BaseModel
 from database import Base
+from typing import Annotated
+from pydantic import BaseModel, Field
+from sqlalchemy import Column, Integer, String
+
+class UserModel(Base):
+    '''
+    the UserModel class is inheriting from database.py Base class to create database tables for users
+    '''
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, index=True, unique=True)
+    password = Column(String)
+    email = Column(String, index=True, unique=True)
+    full_name = Column(String, nullable=True)
+    address = Column(String)
+    phone_number = Column(String)
+
+
+class User(BaseModel):
+    '''
+    the User class is inheriting from pydantic BaseModel for validation
+    '''
+    username: Annotated[str, Field(min_length=3, max_length=20)]
+    password: Annotated[str, Field(min_length=8, max_length=72)]
+    email: Annotated[str, Field(max_length=100)]
+    full_name: Annotated[str, Field(max_length=100)] | None = None
+    address: Annotated[str, Field(min_length=5, max_length=250)]
+    phone_number: Annotated[str, Field(min_length=10, max_length=10)]
+
+    class Config:
+        from_attributes = True
